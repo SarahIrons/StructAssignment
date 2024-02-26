54ed1ea7b36742d537899f9b08685bcecab4da9d
SarahIrons <sarahirons11@gmail.com>
2024-02-26 11:46:44 -0500

    Add project files.

---
+++ StructAssignment/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    public struct Number
+    {
+       public decimal Amount; //Declared a public property of decmial type called Amount
+    }
+}
---
+++ StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//STRUCT ASSIGNMENT
+
+//Perform these actions and create a console app that includes the following:
+
+
+//Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
+
+//In the Main() method, create an object of data type Number and assign an amount to it.
+
+//Print this amount to the console.
+namespace StructAssignment
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Number cost = new Number(); //instantiate new instance of Number
+            cost.Amount = 34.56M; //designate that this is decimal with "M" at end of the number
+          Console.WriteLine(cost.Amount); //variable name 'cost' put through struct (cost.Amount) to output the reading of 34.56
+        }
+    }
+}
